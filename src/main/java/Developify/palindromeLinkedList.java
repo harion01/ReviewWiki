5a96094fd9368cef99d3c68bf7f007e47af8a2ec
Chris Lee <jjlee@bistel.com>
2021-03-07 18:15:47 +0900

    compare two linked list

--- src/main/java/Developify/palindromeLinkedList.java
+++ src/main/java/Developify/palindromeLinkedList.java
@@ -1,7 +1,7 @@
 package Developify;
 
 
-
+import java.util.ArrayList;
 
 public class palindromeLinkedList {
     class ListNode {
@@ -11,8 +11,34 @@ public class palindromeLinkedList {
         ListNode(int val) { this.val = val; }
     }
 
-    public boolean isPalindrome(ListNode head) {
+    public static void main(String args[]){
+        ArrayList<Integer> initArr = new ArrayList<>();
+        initArr.add(1);
+        initArr.add(2);
+        initArr.add(2);
+        initArr.add(1);
 
+        palindromeLinkedList runner = new palindromeLinkedList();
+        ListNode node = runner.initList(initArr);
+        runner.printList(node);
+
+        //runner.isPalindrome(node);
+        runner.isPalindromeReduceSpace(node);
+    }
+
+    public ListNode initList(ArrayList<Integer> data){
+        ListNode node = new ListNode();
+        ListNode head = node;
+        node.val = data.get(0);
+        for(int idx = 1 ; idx < data.size(); idx++){
+            ListNode nextNode = new ListNode(data.get(idx));
+            node.next = nextNode;
+            node = node.next;
+        }
+        return head;
+    }
+
+    public boolean isPalindrome(ListNode head) {
         ListNode node = head;
         int nodeSize = 0;
         while(node != null){
@@ -40,6 +66,78 @@ public class palindromeLinkedList {
 
         System.out.println("nodeSize: "+nodeSize+" first :"+firstWord+"second :"+secondWord);
         return firstWord.equals(secondWord);
+    }
+
+    public boolean isPalindromeReduceSpace(ListNode head) {
+        ListNode node = head;
+        int nodeSize = 0;
+        while(node != null){
+            nodeSize++;
+            node = node.next;
+        }
+        node = head;
+
+        int middle = nodeSize/2;
+        for(int firstIdx = 0 ; firstIdx < middle ; firstIdx++ ){
+            node = node.next;
+        }
+
+        ListNode curNode = node;
+        if(nodeSize%2 != 0 && nodeSize>2){
+            curNode = curNode.next;
+        }
+
+        ListNode backwardNode = makeReverseLinkedList(curNode);
+        //System.out.println("reverse node");
+        //printList(backwardNode);
+
+        ListNode forwardNode =  makeFirstNode(node, middle);
+        //System.out.println("first node");
+        //printList(forwardNode);
+        for(int count = 0 ; count < middle; count++){
+           // System.out.println("compare node - first :"+forwardNode.val + " reverse:"+backwardNode.val);
+            if(forwardNode.val != backwardNode.val){
+                return false;
+            }
+            forwardNode = forwardNode.next;
+            backwardNode = backwardNode.next;
+        }
+        return true;
+    }
+
+    private ListNode makeReverseLinkedList(ListNode curNode) {
+        ListNode reverseNode = curNode.next;
+        ListNode prevNode = curNode;
+        prevNode.next = null;
+
+        while(reverseNode != null){
+            curNode = reverseNode;
+            reverseNode = reverseNode.next;
+            curNode.next = prevNode;
+            prevNode = curNode;
+        }
+        reverseNode = curNode;
+        return reverseNode;
+    }
+
+    private ListNode makeFirstNode(ListNode node, int middle) {
+        ListNode firstNode = node;
+        ListNode moveNode =node;
+        for(int firstIdx = 0 ; firstIdx < middle-1 ; firstIdx++ ){
+            moveNode = moveNode.next;
+        }
+        moveNode.next = null;
+        return firstNode;
+    }
+
+    public void printList(ListNode head){
+        ListNode node = head;
+        System.out.print("Print node : ");
+        while(node != null){
+            System.out.print(" - "+node.val);
+            node = node.next;
+        }
+        System.out.println("");
     }
 
 }
---
+++ src/main/java/Theory/algorithm/sort/mergeSort.java
@@ -0,0 +1,75 @@
+package Theory.algorithm.sort;
+
+import java.util.ArrayList;
+
+public class mergeSort {
+
+    public static void main (String args[]){
+        ArrayList<Integer> origin = new ArrayList<>();
+        origin.add(1);
+        origin.add(3);
+        origin.add(7);
+        origin.add(5);
+
+        mergeSort sort = new mergeSort();
+        ArrayList<Integer> sorted = sort.invoke(origin);
+
+        System.out.println("origin : "+origin);
+        System.out.println("sorted : "+sorted);
+    }
+
+    public ArrayList<Integer> invoke(ArrayList<Integer> original){
+        ArrayList<Integer> sorted = null;
+        sorted = sort(original, 0, original.size());
+        return sorted;
+    }
+
+    private ArrayList<Integer> sort(ArrayList<Integer> target, int startIdx, int endIdx) {
+        System.out.println("sort called. start : "+ startIdx + " end : " +endIdx);
+        int gap = (endIdx - startIdx);
+        ArrayList<Integer> left =null;
+        ArrayList<Integer> right = null;
+        ArrayList<Integer> mergeSorted = new ArrayList<>();
+
+        if(gap >= 1) {
+            int half = gap/2;
+            left = sort(target, startIdx, startIdx+half);
+            right = sort(target, endIdx-half, endIdx);
+        }else if(gap == 0){
+            mergeSorted.add(target.get(startIdx));
+            System.out.println("return init merged : "+mergeSorted);
+            return mergeSorted;
+        }
+        System.out.println("left : "+left);
+        System.out.println("right : "+right);
+
+        int leftIdx = 0;
+        int rightIdx = 0;
+        int leftItem = 0;
+        int rightItem = 0;
+        while(leftIdx < left.size() && rightIdx < right.size() ){
+            leftItem = left.get(leftIdx);
+            rightItem = right.get(rightIdx);
+            if(leftItem < rightItem){
+                mergeSorted.add(left.get(leftIdx));
+                leftIdx++;
+            }else {
+                mergeSorted.add(right.get(rightIdx));
+                rightIdx++;
+            }
+        }
+        System.out.println(" merged : "+mergeSorted);
+
+        for(int idx = leftIdx ; idx < left.size() ; idx++){
+            mergeSorted.add(left.get(idx));
+        }
+        for(int idx = rightIdx ; idx < right.size() ; idx++){
+            mergeSorted.add(right.get(idx));
+        }
+
+        System.out.println("return  merged : "+mergeSorted);
+        return mergeSorted;
+    }
+
+
+}
