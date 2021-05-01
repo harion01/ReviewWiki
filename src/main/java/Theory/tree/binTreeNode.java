5e5056251a9daba0246f742e965bcbe71d31ad8e
harion01 <harion01@gmail.com>
2021-05-01 22:32:58 +0900

    implement normal bintree

---
+++ src/main/java/Theory/tree/binTreeNode.java
@@ -0,0 +1,13 @@
+package Theory.tree;
+
+public class binTreeNode {
+    int value;
+    binTreeNode leftChild;
+    binTreeNode rightChild;
+
+    binTreeNode(int val){
+        value = val;
+        leftChild = null;
+        rightChild = null;
+    }
+}
--- src/main/java/Theory/tree/binaryTree.java
+++ src/main/java/Theory/tree/binaryTree.java
@@ -1,5 +1,79 @@
 package Theory.tree;
 
 public class binaryTree {
-    
+    private binTreeNode root = null;
+
+    binaryTree(binTreeNode node){
+        root = node;
+    }
+
+    public boolean search(int val){
+        binTreeNode checkNode = root;
+        while(checkNode != null){
+            if (checkNode.value == val){
+                return true;
+            } else if (checkNode.value > val){
+                checkNode = checkNode.leftChild;
+            } else if (checkNode.value < val){
+                checkNode = checkNode.rightChild;
+            }
+        }
+        return false;
+    }
+
+    public void insert(int val){
+        binTreeNode newNode = new binTreeNode(val);
+        binTreeNode checkNode = root;
+        while(checkNode != null){
+            if(checkNode.value > newNode.value){
+                if(checkNode.leftChild != null){
+                    checkNode = checkNode.leftChild;
+                } else {
+                    checkNode.leftChild = newNode;
+                    break;
+                }
+
+            } else if (checkNode.value < newNode.value) {
+                if(checkNode.rightChild != null){
+                    checkNode = checkNode.rightChild;
+                } else {
+                    checkNode.rightChild = newNode;
+                    break;
+                }
+            }
+        }
+
+    }
+
+    public void delete(int val){
+        binTreeNode checkNode = root;
+
+        while(checkNode != null){
+            if (checkNode.value == val){
+                rebalanceTree(checkNode);
+            } else if (checkNode.value > val){
+                checkNode = checkNode.leftChild;
+            } else if (checkNode.value < val){
+                checkNode = checkNode.rightChild;
+            }
+        }
+
+    }
+
+    private void rebalanceTree(binTreeNode removedNode){
+        if(removedNode.rightChild != null ){
+
+            binTreeNode leftMostParent = removedNode.rightChild;
+            while(leftMostParent.leftChild != null){
+                leftMostParent = leftMostParent.leftChild;
+            }
+            leftMostParent.leftChild = removedNode.leftChild;
+
+            removedNode = removedNode.rightChild;
+        } else {
+            removedNode = removedNode.leftChild;
+        }
+
+    }
+
 }
